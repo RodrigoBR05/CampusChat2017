1a0f7309fa2502f3c64521974ec871cc22a5c73a
RodrigoBR05 <rodri_bre05@hotmail.es>
2017-09-21 10:54:02 -0600

    Jueves 21 de setiembre del 2017

--- api/controllers/MensajesController.js
+++ api/controllers/MensajesController.js
@@ -8,10 +8,10 @@
 module.exports = {
 
 	create:function(req,res){
-        var mensaje = req.param('mensaje');
-        var transmisor = req.param('transmisor');
-        var receptor = req.param('receptor');
-        var grupo = req.param('grupo');
+        var mensaje = req.param('contenido');
+        var transmisor = req.param('id_transmisor');
+        var receptor = req.param('id_receptor');
+        var grupo = req.param('id_curso');
 
         //Crear el mensaje
         if (receptor) {        	
@@ -48,6 +48,64 @@ module.exports = {
                 });                
             });
         }
+    },
+
+    mensajes:function(req,res){        
+        var transmisor = req.param('transmitter');
+        var receptor = req.param('receiver');
+        var grupo = req.param('group');
+        //var roomName = req.param('roomName');
+
+        //Subcribirse al socket
+        if( ! req.isSocket) {
+          return res.badRequest();
+        }
+
+        //Suscripción al socket
+        sails.sockets.join(req.socket, 'roomName',function(err){
+            if (err) {
+                return res.serverError(err);
+            }
+            //console.log('Inscrito en la room'+roomName);
+        });
+        
+        //Buscar la forma de integrar el grupo
+        if (receptor) {
+            //Recopilo los mensajes del usuario desde la relación User-Message
+            Mensajes.find({
+                //Mensajes recibidos o enviados de o hacia el usuario actual
+                or: [
+                    { id_transmisor:transmisor, id_receptor:receptor},
+                    { id_transmisor:receptor, id_receptor:transmisor}
+                ]
+            ,sort: 'id_mensaje ASC'}).populate('id_receptor').populate('id_transmisor').exec(function (err, messages){
+              if (err) {
+                return res.serverError(err);
+              }
+              if (!messages) {
+                return res.notFound('No existen registros de mensajes.');
+              }
+
+              //sails.log('Registro encontrado:', message);
+              return res.json(messages);
+            });          
+        }
+        //Si no existe receptor se debe buscar en el grupo seleccionado
+        else if (grupo){
+            //Recopilo los mensajes de ese grupo
+            Message.find({group: grupo,sort: 'id DESC'}).populate('transmitter').exec(function (err, message){
+              if (err) {
+                return res.serverError(err);
+              }
+              if (!message) {
+                return res.notFound('No existen registros de mensajes.');
+              }
+
+              //sails.log('Registro encontrado:', message);
+              return res.json(message);
+            });  
+        }
+
     }
 	
 };
